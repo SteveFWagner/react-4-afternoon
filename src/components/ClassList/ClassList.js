27cd43807a20d49d72a0a80b51b46ef4fb62b346
SteveFWagner <sfwagner1011@gmail.com>
2019-02-12 15:21:45 -0700

    complete

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props)
+    this.state={
+      students:[]
+    }
+  }
+
+  componentDidMount(){
+    axios.get(`http://localhost:3005/students?class=${this.props.match.params.class}`).then(res=>{
+      // console.log(res.data)
+      this.setState({
+        students: res.data
+      })
+    })
+    console.log("match.props @ ClassList", this.props.match.params.class)
   }
 
   render() {
+    let mapped = this.state.students.map((val,i,arr) => {
+      let {first_name, last_name, id} = val
+      return(
+        <Link to={`/student/${id}`}>
+          <h3 >
+            {first_name} {last_name}
+          </h3>
+        </Link>
+      )
+    })
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{this.props.match.params.class}</h1>
         <h2>ClassList:</h2>
-
+          {mapped}
+        <Link to="/"><button><h1>BACK TO HOME!</h1></button></Link>
       </div>
     )
   }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
 
+
+ 
 export default class Student extends Component {
   constructor() {
     super()
+    this.state={
+      studentInfo:{},
+      class:``
+    }
+  }
 
+  componentDidMount(){
+
+    axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res=>{
+      console.log(res)
+      this.setState({
+        studentInfo: res.data,
+        class: res.data.class
+      })
+    })
+    // console.log("Current state @ student",this.state)
   }
 
   render() {
+    let {first_name, last_name, grade,email} = this.state.studentInfo
+    
+    console.log(this.props)
     return (
       <div className="box">
         <h1>Student</h1>
+        <h1>{first_name} {last_name}</h1>
+        <h3>Grade: {grade}</h3>
+        <h3>Email: {email}</h3>
+        <Link to={`/classlist/${this.state.class}`}><button><h1>BACK!</h1></button></Link>
       </div>
     )
   }
